6f2062553adc0bdd756ca349224c2e8a9caee33d
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2022-12-10 18:16:19 -0800

    took out rainbow on click

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 A fun Star Wars Project.  
 Founds a great Neon Font that flickers when you click on the letters.
 
+live site: https://lambent-jalebi-6f3ada.netlify.app/
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {useState, useEffect} from 'react';
 import './App.css';
 import Starships from './Components/Starships';
 
+
 function App() {
   const [starshipData, setStarshipData] = useState(null);
 
@@ -35,6 +36,7 @@ function App() {
       backgroundRepeat: 'no-repeat'
     }} >
         <h1 className="glow">STAR WARS STARSHIPS</h1>
+        
    
         <section className="container" >
           {starships}
@@ -45,23 +47,10 @@ function App() {
      
       
     </div>
+
     
   );
+ 
 }
 
-const target = window.document.getElementsByTagName('h1')[0]
-
-const flickerLetter = letter => `<span style="animation: text-flicker-in-glow ${Math.random()*4}s linear both ">${letter}</span>`
-const colorLetter = letter => `<span style="color: hsla(${Math.random()*360}, 100%, 80%, 1);">${letter}</span>`;
-const flickerAndColorText = text => 
-  text
-    .split('')
-    .map(flickerLetter)
-    .map(colorLetter)
-    .join('');
-const neonGlory = target => target.innerHTML = flickerAndColorText(target.textContent);
-
-
-neonGlory(target);
-target.onclick = ({ target }) =>  neonGlory(target);
 export default App;
